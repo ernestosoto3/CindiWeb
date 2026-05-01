d29a8034c391725e526fd707f29124c155ba407d
ernestosoto3 <ernesto.soto3@upr.edu>
2026-04-30 21:06:24 -0400

    cindiweb 2.7.1.0: feat - update other el-tenedor client files before deployment

--- clients/el-tenedor/app/layout.tsx
+++ clients/el-tenedor/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
     <html lang="es">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
           href="https://fonts.googleapis.com/css2?family=Alex+Brush&family=Cinzel:wght@400;500;600&family=Crimson+Pro:ital,wght@0,300;0,400;0,500;1,300;1,400&family=Raleway:wght@300;400;500;600&display=swap"
           rel="stylesheet"
